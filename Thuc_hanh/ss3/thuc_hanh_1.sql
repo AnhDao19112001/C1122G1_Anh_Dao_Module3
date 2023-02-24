4694b645f4a91dd70cb9c6d46bb8495e85a6ce48
Anh Đào <phantaanhdao@gmail.com>
2023-02-24 13:36:19 +0700

    first commit

--- Thuc_hanh/ss3/thuc_hanh_1.sql
+++ Thuc_hanh/ss3/thuc_hanh_1.sql
@@ -2,7 +2,7 @@ drop database if exists quan_li_sinh_vien;
 create database quan_li_sinh_vien;
 use quan_li_sinh_vien;
 create table class(
-class_id int primary key auto_increment,
+class_id int primary key,
 class_name varchar(60) not null,
 start_date datetime not null,
 `status` bit
@@ -35,3 +35,23 @@ subject_id int,
 foreign key (student_id) references student(student_id),
 foreign key (subject_id) references `subject`(subject_id)
 );
+
+insert into class(class_id,class_name,start_date,`status`)
+values (1,'c1122g1','2022-12-01',1),
+(2,'c0123g1','2023-01-01',1),
+(3,'c1022g1','2022-10-28',1);
+
+
+insert into student(student_id,student_name,address,phone,`status`,class_id)
+values (1,'Anh Đào','Ái Nghĩa, Đại Lộc, Quảng Nam', 0931997293,1,1),
+(2,'Huấn Hoa Hồng','Phu Ninh, Quảng Nam',123456789,1,3),
+(3, 'Hung','Da Nang',987654321,1,3);
+
+insert into `subject`(subject_id,subject_name,credit,`status`)
+values (1,'bla bla',1,1),
+(2,'dark dark',5,1),
+(3,'burh burh',3,1),
+(4,'lmao lmao',10,1);
+
+insert into mark(mark_id,mark,exam_times,student_id,subject_id)
+values (1,1,1,1,1);
